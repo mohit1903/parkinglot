98228b771691a522a1c78b7c2c5906b9fa0e8e1a
Mohit <mohit190392@gmail.com>
2019-02-02 21:48:28 +0530

    Entity and Constant added

---
+++ ParkingLot/src/main/java/com/gojek/constant/ConstantCommand.java
@@ -0,0 +1,44 @@
+package com.gojek.constant;
+
+public enum ConstantCommand {
+    CREATE("create_parking_lot"),
+    PARK("park"),
+    LEAVE("leave"),
+    STATUS("status"),
+    FETCH_CARE_FROM_COLOR("registration_numbers_for_cars_with_colour"),
+    FETCH_SLOT_FROM_COLOR("slot_numbers_for_cars_with_colour"),
+    FETCH_SLOT_FROM_REG_NO("slot_number_for_registration_number");
+    
+    private final String name;  
+	
+	private ConstantCommand(String s) {
+        name = s;
+    }
+	
+	public static ConstantCommand findByName(String abbr){
+	    for(ConstantCommand c : values()){
+	        if( c.toString().equals(abbr)){
+	            return c;
+	        }
+	    }
+	    return null;
+	}
+	
+	public boolean equalsName(String otherName) {
+        return name.equals(otherName);
+    }
+
+    public String toString() {
+       return name;
+    }
+    
+    public static <T extends Enum<T>> T getEnumFromString(Class<T> c, String string) {
+        if( c != null && string != null ) {
+            try {
+                return Enum.valueOf(c, string.trim().toUpperCase());
+            } catch(IllegalArgumentException ex) {
+            }
+        }
+        return null;
+    }
+}
